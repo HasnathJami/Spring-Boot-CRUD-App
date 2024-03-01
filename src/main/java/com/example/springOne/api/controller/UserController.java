4b6d05ca074ff5b396749d42b7ce707c975f84b0
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-03-02 03:13:58 +0600

    Crud Implementation

--- src/main/java/com/example/springOne/api/controller/UserController.java
+++ src/main/java/com/example/springOne/api/controller/UserController.java
@@ -3,25 +3,38 @@ package com.example.springOne.api.controller;
 import com.example.springOne.api.model.User;
 import com.example.springOne.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
-class UserController {
+public class UserController {
 
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/user")
-    public User getUser(@RequestParam Integer id){
+    public User getUser(@RequestParam Integer id) {
         Optional<User> user = userService.getUser(id);
-        return (User) user.orElse(null);
+        return user.orElse(null);
+    }
+
+    @PostMapping("/user/create")
+    public User createUser(@RequestBody User newUser) {
+        return userService.createUser(newUser);
+    }
+
+    @PutMapping("/user/update/{id}")
+    public User updateUser(@PathVariable Integer id, @RequestBody User updatedUser) {
+        return userService.updateUser(id, updatedUser);
+    }
+
+    @DeleteMapping("/user/delete/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userService.deleteUser(id);
     }
 }
--- src/main/java/com/example/springOne/service/UserService.java
+++ src/main/java/com/example/springOne/service/UserService.java
@@ -4,7 +4,6 @@ import com.example.springOne.api.model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,22 +15,40 @@ public class UserService {
     public UserService() {
         userList = new ArrayList<>();
 
-        User user1 = new User(1,"Jishan", 27, "j@gmail.com");
-        User user2 = new User(2,"Hasnath", 26, "hasnatj@gmail.com");
-        User user3 = new User(3,"Jami", 25, "jami@gmail.com");
-        User user4 = new User(4,"Chowdhury", 24, "chowdhury@gmail.com");
+        // Initialize the list with some dummy users
+        userList.add(new User(1, "Jishan", 27, "j@gmail.com"));
+        userList.add(new User(2, "Hasnath", 26, "hasnatj@gmail.com"));
+        userList.add(new User(3, "Jami", 25, "jami@gmail.com"));
+        userList.add(new User(4, "Chowdhury", 24, "chowdhury@gmail.com"));
+    }
 
-        userList.addAll(Arrays.asList(user1,user2,user3,user4));
+    public List<User> getAllUsers() {
+        return userList;
     }
 
     public Optional<User> getUser(Integer id) {
-        Optional<User> optional = Optional.empty();
-        for (User user: userList) {
-            if(id == user.getId()){
-                optional = Optional.of(user);
-                return optional;
-            }
+        return userList.stream().filter(user -> user.getId() == id).findFirst();
+    }
+
+    public User createUser(User newUser) {
+        userList.add(newUser);
+        return newUser;
+    }
+
+    public User updateUser(Integer id, User updatedUser) {
+        Optional<User> userOptional = getUser(id);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setName(updatedUser.getName());
+            user.setAge(updatedUser.getAge());
+            user.setEmail(updatedUser.getEmail());
+            return user;
+        } else {
+            throw new RuntimeException("User not found with id: " + id);
         }
-        return optional;
+    }
+
+    public void deleteUser(Integer id) {
+        userList.removeIf(user -> user.getId() == id);
     }
 }
